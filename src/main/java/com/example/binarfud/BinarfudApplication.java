133e2cffa7690f5aac6a4e820f879453e74ed2a8
Michelle Chan <mc.chellechan@gmail.com>
2024-05-31 17:57:11 +0800

    Remove propertysource

--- src/main/java/com/example/binarfud/BinarfudApplication.java
+++ src/main/java/com/example/binarfud/BinarfudApplication.java
@@ -4,12 +4,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@PropertySource("classpath:secrets.properties")
 public class BinarfudApplication {
 
 	public static void main(String[] args) throws InterruptedException {
